31d1b1969c97505b001e4987aa8da1cc79fa4d02
weili <weili85290627452@163.com>
2021-10-27 14:57:09 +0800

    把联系人改成Mr.lee；测试邮件发送

--- testEmail.php
+++ testEmail.php
@@ -1,19 +1,20 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: weili
- * Date: 2021/4/24
- * Time: 下午5:41
- * Description:
+ * User: Administrator
+ * Date: 2021/4/23
+ * Time: 15:17
  */
 
 if(!$_POST['email']){
     echo json_encode(array('error_code'=>1,'error_msg'=>"邮箱地址必填！"));exit;
 }
-echo postmail('aksoherb@hotmail.com',$_POST['subject'],$_POST['message'].'<br /> 邮箱：'.$_POST['email']);exit;
 
-function postmail($to,$subject = '',$body = ''){
+echo postmail_test('suyi1059882348@qq.com',$_POST['subject'],$_POST['message'].'<br /> 邮箱：'.$_POST['email']);exit;
+
+function postmail_test($to,$subject = '',$body = ''){
     //$to 表示收件人地址 $subject 表示邮件标题 $body表示邮件正文
+//    error_reporting(E_STRICT);
     date_default_timezone_set('Asia/Shanghai');//设定时区东八区
     require_once('phpmail/class.phpmailer.php');
     include('phpmail/class.smtp.php');
@@ -25,16 +26,17 @@ function postmail($to,$subject = '',$body = ''){
     // 2 = messages only
     $mail->SMTPAuth   = true;                  // 启用 SMTP 验证功能
     $mail->SMTPSecure = "ssl";                 // 安全协议，可以注释掉
-    $mail->Host       = 'smtp.163.com';      // SMTP 服务器
+    $mail->Host       = 'mail.sohu.com';      // SMTP 服务器
     $mail->Port       = 465;                   // SMTP服务器的端口号       587/465/25
-    $mail->Username   = 'wangrain2021@163.com';  // SMTP服务器用户名，账号就是邮箱号
-    $mail->Password   = 'TVISKJXPPFQFXNCC';            // SMTP服务器密码  授权码
-    $mail->SetFrom('wangrain2021@163.com', 'blue');   // 发件人的邮箱地址和昵称
+    $mail->Username   = 'sunnyjia@sohu.com';  // SMTP服务器用户名，账号就是qq邮箱号
+    $mail->Password   = 'K98GQHXE35AZJ';            // SMTP服务器密码  那个授权码
+    $mail->SetFrom('sunnyjia@sohu.com', 'blue');   // 发件人的邮箱地址和昵称
     $mail->Subject    = $subject;
     $mail->AltBody    = 'To view the message, please use an HTML compatible email viewer!'; // optional, comment out and test
     $mail->MsgHTML($body);
     $address = $to;
     $mail->AddAddress($address, '');
+//    $mail->AddAttachment("images/403.jpg");      // attachment 附件
     $mail->IsHTML(true); //是否使用HTML格式
     if(!$mail->Send()) {
         return json_encode(array('error_code'=>1,'error_msg'=>'Mailer Error: ' . $mail->ErrorInfo));
